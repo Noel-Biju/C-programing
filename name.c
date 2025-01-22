a326c4e42251e25797201794952c264b5ac6d97f
Noel-Biju <noelbiju2028@cs.sjcetpalai.ac.in>
2025-01-22 15:44:46 +0530

    Add files via upload

--- name.c
+++ name.c
@@ -2,7 +2,8 @@
 int main(){
   char name[20];
   printf("\nEnter your name:");
-  scanf("%s",name);
+  //scanf("%s",name);
+  fgets(name,sizeof(name),stdin);
   printf("Welcome %s",name);
   return 0;
 }
