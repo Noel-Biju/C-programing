5780c3b2ad08a98623408d339503d7c381a34f80
Noel-Biju <noelbiju2028@cs.sjcetpalai.ac.in>
2025-01-29 14:54:42 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+  int num1,num2,sum=0;
+  char operator;
+  printf("\nEnter the first number:");
+  scanf("%d",&num1);
+  printf("\nEnter the second number:");
+  scanf("%d",&num2);
+  printf("\nEnter one of the operators(+,-,*,/,%%):");
+  scanf(" %c",&operator);
+  switch(operator){
+    case '+':
+    printf("%d+%d=%d",num1,num2,num1+num2);
+    break;
+    case '-':
+    printf("%d-%d=%d\n",num1,num2,num1-num2);
+    break;
+    case '*':
+    printf("%d*%d=%d\n",num1,num2,num1*num2);
+    break;
+    case '/':
+    printf("%d/%d=%d\n",num1,num2,num1/num2);
+    break;
+    case '%':
+    printf("%d %% %d=%d\n",num1,num2,num1%num2);
+    break;
+    default:
+    printf("\nInvalid operator");
+  }
+  return 0;
+}
