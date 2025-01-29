9b0e891251460d099a69ad75e4fa31b109796890
Noel-Biju <noelbiju2028@cs.sjcetpalai.ac.in>
2025-01-29 14:55:33 +0530

    Add files via upload

--- calculator.c
+++ calculator.c
@@ -10,7 +10,7 @@ int main(){
   scanf(" %c",&operator);
   switch(operator){
     case '+':
-    printf("%d+%d=%d",num1,num2,num1+num2);
+    printf("%d+%d=%d\n",num1,num2,num1+num2);
     break;
     case '-':
     printf("%d-%d=%d\n",num1,num2,num1-num2);
